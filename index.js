333fec2718313ffc25a624b85c4b7144857ce93b
jayasree012 <66725625+jayasree012@users.noreply.github.com>
2021-05-31 17:46:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function makeSound(key)
     case "s":var tom3=new Audio("sounds/tom-3.mp3");tom3.play();break;
     case "d":var tom4=new Audio("sounds/tom-4.mp3");tom4.play();break;
     case "j":var crash=new Audio("sounds/crash.mp3");crash.play();break;
-    case "k":var kick=new Audio("sounds/kick.mp3");kick.play();break;
+    case "k":var kick=new Audio("sounds/kick-bass.mp3");kick.play();break;
     case "l":var snare=new Audio("sounds/snare.mp3");snare.play();break;
 
     default:
